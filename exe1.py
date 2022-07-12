d693891593f5829be1d923e2db4d5477db622b3d
SathiyamV <sathiyamsharmila@gmail.com>
2022-07-12 13:24:18 +0530

    first commit

---
+++ exe1.py
@@ -0,0 +1,4 @@
+patient_name = "yhoshito"
+age = 20
+new_patient = True
+print(patient_name,age,new_patient)
---
+++ exe2.py
@@ -0,0 +1,4 @@
+first_no=input("Enter your first no: ")
+second_no=input("Enter your seconnd no: ")
+total=float(first_no)+float(second_no)
+print(total)
